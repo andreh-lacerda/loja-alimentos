0a3e738f704675397cdf9f33e680ed8e391a8ac5
Andre Lacerda <andreh.lacerda@hotmail.com>
2021-11-03 10:34:54 -0300

    add. error-dialog

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,7 +1,10 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { ProductsService } from './../service/products.service';
 import { Product } from './../model/product';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-products',
@@ -14,10 +17,23 @@ export class ProductsComponent implements OnInit {
   displayedColumns = ['name', 'quantity', 'price'];
 
 
-  constructor(private productsService: ProductsService) {
-    //this.productsService = new ProductsService();
-    this.products$ = this.productsService.list();
+  constructor(
+    private productsService: ProductsService,
+    public dialog: MatDialog,
+    ) {
+    this.products$ = this.productsService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar os produtos.')
+        return of([])
+      })
+    );
+  }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
